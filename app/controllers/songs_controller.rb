98d299704276a65751889bdc6d5b7891848f1f8f
AudreyJones <audrey.eliz.jones@gmail.com>
2019-04-24 14:26:13 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,33 @@
+class SongsController < ApplicationController
+ef new
+        @song = Song.new
+    end
+
+        def create
+            @song = Song.new(song_params(:name, :bio))
+            @song.save
+            redirect_to artist_path(@song)
+        end
+
+        def show
+            @song = Song.find(params[:id])
+            # binding.pry
+        end
+
+        def edit
+            @song = Song.find(params[:id])
+        end
+
+        def update
+            @song = Song.find(params[:id])
+            @song.update(song_params(:name, :bio))
+            # binding.pry
+            redirect_to artist_path(@song)
+        end
+
+    private
+
+    def song_params(*args)
+        params.require(:song).permit(*args)
+    end
+end
