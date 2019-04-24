265c52f967f4282f5bdcb5d1b1c62800184d06c6
TaylorSwinton <taylorswinton1@gmail.com>
2019-04-24 13:44:56 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,14 @@
+class SongsController < ApplicationController
+    def create
+        
+    end
+
+    def update
+        
+    end
+
+    def show
+        #display each of the songs genre and artist
+        #link to the respective genre and artist show pages
+    end
+end
